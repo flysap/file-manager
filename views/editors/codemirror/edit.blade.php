0c89615179bac8607a63c7b92c619e72f597bdc1
Radu <radu.bordei89@gmail.com>
2015-07-29 14:23:55 +0300

    minor fixes.

--- views/editors/codemirror/edit.blade.php
+++ views/editors/codemirror/edit.blade.php
@@ -6,21 +6,13 @@
 <script src="/assets/codemirror/addon/selection/active-line.js"></script>
 <script src="/assets/codemirror/addon/edit/matchbrackets.js"></script>
 
-<style type="text/css">
-    .CodeMirror {
-        border: 1px solid #eee;
-        height: auto;
-    }
-</style>
-
 <script type="text/javascript">
     @if(isset($attributes['on_click']))
         $("{{ $attributes['on_click'] }}").on("click", function() {
     @endif
-
         setTimeout(function() {
-           if(! window.codeEditor) {
-               window.codeEditor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+           if(! window.{{isset($attributes['editor_var']) ? $attributes['editor_var']  :'coreEditor'}}) {
+               window.{{isset($attributes['editor_var']) ? $attributes['editor_var']  :'coreEditor'}} = CodeMirror.fromTextArea(document.getElementById("editor"), {
                    lineNumbers: true,
                    styleActiveLine: true,
                    matchBrackets: true,
--- views/select.blade.php
+++ views/select.blade.php
@@ -1,8 +1,6 @@
 <?php $active = isset($attributes['active']) ? $attributes['active']  :'' ?>
 <select name="{{isset($attributes['name']) ? $attributes['name']  :'file'}}" class="{{isset($attributes['class']) ? $attributes['class']  :''}}">
     @foreach($files as $file)
-    <option value="{{$file->getRelativePathname()}}" @if($active == $file->getRelativePathname()) selected="selected" @endif>
-        {{$file->getRelativePathname()}}
-    </option>
+    <option value="{{$file->getRelativePathname()}}" @if($active == $file->getRelativePathname()) selected="selected" @endif>{{$file->getRelativePathname()}}</option>
     @endforeach
 </select>
