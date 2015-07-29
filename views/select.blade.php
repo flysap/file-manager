b43f2daebe4c79f5bd931378cc2a37ddd391ec2e
Radu <radu.bordei89@gmail.com>
2015-07-29 13:45:30 +0300

    remove form and button.

--- views/select.blade.php
+++ views/select.blade.php
@@ -1,12 +1,8 @@
-<form method="get">
-    <?php $active = isset($attributes['active']) ? $attributes['active']  :'' ?>
-    <select name="file">
-        @foreach($files as $file)
-        <option value="{{$file->getRelativePathname()}}" @if($active == $file->getRelativePathname()) selected="selected" @endif>
-            {{$file->getRelativePathname()}}
-        </option>
-        @endforeach
-    </select>
-
-    <input type="button" value="{{_('Load')}}">
-</form>
+<?php $active = isset($attributes['active']) ? $attributes['active']  :'' ?>
+<select name="{{isset($attributes['name']) ? $attributes['name']  :'file'}}" class="{{isset($attributes['class']) ? $attributes['class']  :''}}">
+    @foreach($files as $file)
+    <option value="{{$file->getRelativePathname()}}" @if($active == $file->getRelativePathname()) selected="selected" @endif>
+        {{$file->getRelativePathname()}}
+    </option>
+    @endforeach
+</select>
