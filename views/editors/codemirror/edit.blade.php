df705945e78fac7da3d0889a128d3e139dad30b0
Radu <radu.bordei89@gmail.com>
2015-07-29 12:33:35 +0300

    refactoring.

--- views/editors/codemirror/edit.blade.php
+++ views/editors/codemirror/edit.blade.php
@@ -1,22 +1,33 @@
 <textarea id="editor">{{$contents}}</textarea>
 
+<link rel="stylesheet" href="/assets/codemirror/lib/codemirror.css">
+<link rel="stylesheet" href="/assets/codemirror//theme/{{$attributes['active_theme']}}.css">
+<script src="/assets/codemirror/lib/codemirror.js"></script>
+<script src="/assets/codemirror/addon/selection/active-line.js"></script>
+<script src="/assets/codemirror/addon/edit/matchbrackets.js"></script>
+
 <style type="text/css">
     .CodeMirror {
         border: 1px solid #eee;
         height: auto;
-        width: 1400px;
     }
 </style>
-<link rel="stylesheet" href="/assets/codemirror/lib/codemirror.css">
-<link rel="stylesheet" href="/assets/codemirror//theme/dracula.css">
-<script src="/assets/codemirror/lib/codemirror.js"></script>
-<script src="/assets/codemirror/addon/selection/active-line.js"></script>
-<script src="/assets/codemirror/addon/edit/matchbrackets.js"></script>
+
 <script type="text/javascript">
-    var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-        lineNumbers: true,
-        styleActiveLine: true,
-        matchBrackets: true,
-        theme: "dracula"
-    });
+    @if(isset($attributes['on_click']))
+        $("{{ $attributes['on_click'] }}").on("click", function() {
+    @endif
+
+        setTimeout(function() {
+           if(! window.codeEditor) {
+               window.codeEditor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+                   lineNumbers: true,
+                   styleActiveLine: true,
+                   matchBrackets: true,
+                   theme: "{{$attributes['active_theme']}}"
+               });
+           }
+        }, 0);
+
+    @if(isset($attributes['on_click'])) }); @endif
 </script>
