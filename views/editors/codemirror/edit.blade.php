f962801c4e4c6253017ab7d39a2289bee1a2c65e
Radu <radu.bordei89@gmail.com>
2015-08-11 17:22:54 +0300

    remove /

--- views/editors/codemirror/edit.blade.php
+++ views/editors/codemirror/edit.blade.php
@@ -1,7 +1,7 @@
 <textarea id="editor">{{$contents}}</textarea>
 
 <link rel="stylesheet" href="/assets/codemirror/lib/codemirror.css">
-<link rel="stylesheet" href="/assets/codemirror//theme/{{$attributes['active_theme']}}.css">
+<link rel="stylesheet" href="/assets/codemirror/theme/{{$attributes['active_theme']}}.css">
 <script src="/assets/codemirror/lib/codemirror.js"></script>
 <script src="/assets/codemirror/addon/selection/active-line.js"></script>
 <script src="/assets/codemirror/addon/edit/matchbrackets.js"></script>
